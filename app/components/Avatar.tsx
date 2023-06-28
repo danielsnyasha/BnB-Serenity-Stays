585358d47831ac5262ffe0f0494fc26ff84e5ede
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-28 14:51:21 +0200

    initial

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const Avatar = () => {
+    return(
+        <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg">
+
+    )
+}
+
+export default Avatar;
---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const Logo = () => {
+//  const router = useRouter();
+
+  return (
+    <Image
+      alt="Logo"
+      className="hidden md:block cursor-pointer"
+      src="/images/logo.png"
+      width="100" // Replace YOUR_IMAGE_WIDTH with the actual width of your image
+      height="100" // Replace YOUR_IMAGE_HEIGHT with the actual height of your image
+    />
+  );
+};
+
+export default Logo;
