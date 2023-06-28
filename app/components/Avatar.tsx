2edffbd381af8493255bfbc38c3b4c1a46cd1ab4
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-28 15:51:03 +0200

    commit2

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,10 +1,9 @@
-'use client';
+import Image from 'next/image';
 
 const Avatar = () => {
-    return(
-        <Image className="rounded-full" height="30" width="30" alt="Avatarr" src="/images/placeholder.jpg">
-
-    )
+    return (
+        <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/logo.png" />
+    );
 }
 
 export default Avatar;
--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = () => {
     <Image
       alt="Logo"
       className="hidden md:block cursor-pointer"
-      src="/images/logo.png"
+      src="/images/eleganza_logo.png"
       width="100" // Replace YOUR_IMAGE_WIDTH with the actual width of your image
       height="100" // Replace YOUR_IMAGE_HEIGHT with the actual height of your image
     />
