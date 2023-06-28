34c69ebb1823a067f684907873f397805ffb6152
Nyasha Musanhu <danielsnyasha@outlook.com>
2023-06-28 15:06:42 +0200

    commitment

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,7 +2,7 @@
 
 const Avatar = () => {
     return(
-        <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg">
+        <Image className="rounded-full" height="30" width="30" alt="Avatarr" src="/images/placeholder.jpg">
 
     )
 }
